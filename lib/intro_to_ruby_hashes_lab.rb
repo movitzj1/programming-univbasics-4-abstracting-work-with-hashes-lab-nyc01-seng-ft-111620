7cb57a67b135624ce5aefb54ec26fd0712910814
Justin Movitz <movitzj1@montclair.edu>
2020-10-21 19:03:40 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -6,7 +6,8 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
+  value = hash[key]
+  return value
 end
 
 def update_counting_hash(hash, key)
